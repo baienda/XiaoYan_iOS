5824f48c32d39a758e3de16f2ac7b6dd59bc5675
bauende <baienda@163.com>
2017-08-09 18:29:31 +0800

    项目

---
+++ 孝颜/孝颜/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  孝颜
+//
+//  Created by benjamin on 2017/8/9.
+//  Copyright © 2017年 com.baienda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 孝颜/孝颜/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  孝颜
+//
+//  Created by benjamin on 2017/8/9.
+//  Copyright © 2017年 com.baienda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
